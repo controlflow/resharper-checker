17b8cd23631fdcdd63afd9c77c9286c3bb5d8a73
ControlFlow <shvedov89@gmail.com>
2013-12-03 04:25:58 +0400

    Decompose YA

--- ReSharper.Checker/ModuleWeaver.cs
+++ ReSharper.Checker/ModuleWeaver.cs
@@ -47,109 +47,131 @@ namespace JetBrains.ReSharper.Checker {
           if (methodDefinition.HasBody) {
             Attributes.CollectFrom(methodDefinition, notNulls);
             if (notNulls.Count > 0) {
-              EmitParametersCheck(methodDefinition, notNulls);
+              EmitMethodEnterChecks(methodDefinition, notNulls);
+              EmitMethodExitChecks(methodDefinition, notNulls);
               notNulls.Clear();
             }
           }
         }
       }
-
     }
 
-    private void EmitParametersCheck(
-      [NotNull] MethodDefinition methodDefinition, [NotNull] HashSet<int> notNulls) {
+    private void EmitMethodEnterChecks(
+      [NotNull] MethodDefinition methodDefinition, [NotNull] HashSet<int> positions) {
 
-      Stack<Instruction[]> inputСhecks = null;
-      Queue<Instruction[]> outputChecks = null;
-      Instruction firstInstruction = null, lastInstruction = null;
+      var inputСhecks = new Stack<Instruction[]>();
+      Instruction firstInstruction = null;
 
-      var parameters = methodDefinition.Parameters; // walk parameters in reverse order
+      // walk over parameters in reverse order to build checks stack
+      var parameters = methodDefinition.Parameters;
       for (var index1 = parameters.Count - 1; index1 >= 0; index1--) {
         var parameterDefinition = parameters[index1];
-        if (notNulls.Contains(parameterDefinition.Index)) {
+        if (parameterDefinition.IsOut) continue;
 
-          var parameterType = parameterDefinition.ParameterType;
-          if (!ChecksEmitUtil.IsNullableType(parameterType))
+        if (!positions.Contains(parameterDefinition.Index)) continue;
+
+        var parameterType = parameterDefinition.ParameterType;
+        if (!ChecksEmitUtil.IsNullableType(parameterType)) {
+          LogInfo(string.Format(
+            "Invalid annotation usage in member {0} at parameter '{1}'.",
+            methodDefinition.GetXmlDocId(), parameterDefinition.Name));
+          continue;
+        }
+
+        if (firstInstruction == null) {
+          var instrictions = methodDefinition.Body.Instructions;
+          if (instrictions.Count == 0) return; // shit happens
+          firstInstruction = instrictions[0];
+        }
+
+        var checkInstructions = ChecksEmitUtil.EmitNullCheckInstructions(
+          parameterDefinition, parameterType, ArgumentNullCtor, firstInstruction,
+          parameterDefinition.Name, "[NotNull] requirement violation");
+
+        inputСhecks.Push(checkInstructions);
+        firstInstruction = checkInstructions[0];
+      }
+
+      if (inputСhecks.Count == 0) return;
+
+      var instructions = methodDefinition.Body.Instructions;
+      var oldBody = instructions.ToArray();
+      instructions.Clear();
+
+      while (inputСhecks.Count > 0)
+        foreach (var instruction in inputСhecks.Pop())
+          instructions.Add(instruction);
+
+      foreach (var instruction in oldBody)
+        instructions.Add(instruction);
+    }
+
+    private void EmitMethodExitChecks(
+      [NotNull] MethodDefinition methodDefinition, [NotNull] HashSet<int> positions) {
+
+      var outputChecks = new Queue<Instruction[]>();
+      Instruction lastInstruction = null;
+
+      // walk over parameters in reverse order to build checks stack
+      var parameters = methodDefinition.Parameters;
+      for (var index1 = parameters.Count - 1; index1 >= 0; index1--) {
+        var parameterDefinition = parameters[index1];
+        if (!positions.Contains(parameterDefinition.Index)) continue;
+
+        var parameterType = parameterDefinition.ParameterType;
+        if (parameterDefinition.IsOut && parameterType.IsByReference) {
+          if (!ChecksEmitUtil.IsNullableType(parameterType)) {
+            LogInfo(string.Format(
+              "Invalid annotation usage in member {0} at parameter '{1}'.",
+              methodDefinition.GetXmlDocId(), parameterDefinition.Name));
             continue;
-
-          if (!parameterDefinition.IsOut) {
-            // init target of null check brtrue jump
-            if (firstInstruction == null) {
-              var instrictions = methodDefinition.Body.Instructions;
-              if (instrictions.Count == 0) return; // shit happens
-              firstInstruction = instrictions[0];
-            }
-
-            var nullCheckInstructions = ChecksEmitUtil.EmitNullCheckInstructions(
-              parameterDefinition, parameterType, ArgumentNullCtor, firstInstruction,
-              parameterDefinition.Name, "[NotNull] requirement contract violation");
-
-            inputСhecks = inputСhecks ?? new Stack<Instruction[]>();
-            inputСhecks.Push(nullCheckInstructions);
-
-            firstInstruction = nullCheckInstructions[0];
           }
 
-          // out parameters
-          if (parameterType.IsByReference) {
-            lastInstruction = lastInstruction ?? Instruction.Create(OpCodes.Ret);
-            var target = (outputChecks != null) ? outputChecks.Peek()[0] : lastInstruction;
+          lastInstruction = lastInstruction ?? Instruction.Create(OpCodes.Ret);
+          var target = (outputChecks.Count > 0) ? outputChecks.Peek()[0] : lastInstruction;
 
-            var nullCheckInstructions2 = ChecksEmitUtil.EmitNullCheckInstructions(
-              parameterDefinition, parameterType, ArgumentNullCtor, target,
-              parameterDefinition.Name, "[NotNull] ensires contract violation");
+          var checkInstructions = ChecksEmitUtil.EmitNullCheckInstructions(
+            parameterDefinition, parameterType, ArgumentNullCtor, target,
+            parameterDefinition.Name, "[NotNull] ensures violation");
 
-            outputChecks = outputChecks ?? new Queue<Instruction[]>();
-            outputChecks.Enqueue(nullCheckInstructions2);
-          }
+          outputChecks.Enqueue(checkInstructions);
         }
       }
 
-      var instructions = methodDefinition.Body.Instructions;
-
-      if (inputСhecks != null) {
-        var oldBody = instructions.ToArray();
-        instructions.Clear();
-
-        while (inputСhecks.Count > 0)
-          foreach (var instruction in inputСhecks.Pop())
-            instructions.Add(instruction);
-
-        foreach (var instruction in oldBody)
-          instructions.Add(instruction);
-      }
-
       var returnType = methodDefinition.MethodReturnType;
-      var emitReturnValueCheck = false;
+      var checkReturnValue = false;
 
-      if (notNulls.Contains(-1) &&
-          ChecksEmitUtil.IsNullableType(returnType.ReturnType) &&
-          !returnType.ReturnType.IsByReference /* ugh! */) {
+      if (positions.Contains(-1)) { // check annotated return
+        if (ChecksEmitUtil.IsNullableType(returnType.ReturnType) && !returnType.ReturnType.IsByReference) {
+          lastInstruction = lastInstruction ?? Instruction.Create(OpCodes.Ret);
+          var target = (outputChecks.Count > 0) ? outputChecks.Peek()[0] : lastInstruction;
 
-        lastInstruction = lastInstruction ?? Instruction.Create(OpCodes.Ret);
-        var target = (outputChecks != null) ? outputChecks.Peek()[0] : lastInstruction;
+          var checkInstructions = ChecksEmitUtil.EmitNullCheckInstructions(
+            null, returnType.ReturnType, ArgumentNullCtor, target,
+            "$return$", "Return value [NotNull] contract violation");
 
-        var checkInstructions = ChecksEmitUtil.EmitNullCheckInstructions(
-          null, returnType.ReturnType, ArgumentNullCtor,
-          target, "$return", "[NotNull] ensures contract violation");
-
-        outputChecks = outputChecks ?? new Queue<Instruction[]>();
-        outputChecks.Enqueue(checkInstructions);
-        emitReturnValueCheck = true;
+          outputChecks.Enqueue(checkInstructions);
+          checkReturnValue = true;
+        } else {
+          LogInfo(string.Format(
+            "Invalid annotation usage in member {0} at return value.", methodDefinition.GetXmlDocId()));
+        }
       }
 
-      if (lastInstruction != null) {
-        var retInstruction = instructions[instructions.Count - 1];
 
-        // replace ret with dup or nop
-        retInstruction.OpCode = emitReturnValueCheck ? OpCodes.Dup : OpCodes.Nop;
-        retInstruction.Operand = null;
+      if (outputChecks.Count == 0) return;
 
-        foreach (var instruction in outputChecks.Dequeue())
-          instructions.Add(instruction);
+      var instructions = methodDefinition.Body.Instructions;
+      var retInstruction = instructions[instructions.Count - 1];
 
-        instructions.Add(lastInstruction);
-      }
+      // replace ret with nop or dup (if return value check required)
+      retInstruction.OpCode = checkReturnValue ? OpCodes.Dup : OpCodes.Nop;
+      retInstruction.Operand = null;
+
+      foreach (var instruction in outputChecks.Dequeue())
+        instructions.Add(instruction);
+
+      instructions.Add(lastInstruction);
     }
   }
 }
